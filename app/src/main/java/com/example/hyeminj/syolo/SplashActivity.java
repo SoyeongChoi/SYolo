27274969c13e4c4559566a7832a40dd007b45473
SoyeongChoi <201602084@cnu.ac.kr>
2018-07-24 14:02:56 +0900

    splash activity 추가(진짜진짜임 이거 됨)

--- app/src/main/java/com/example/hyeminj/syolo/SplashActivity.java
+++ app/src/main/java/com/example/hyeminj/syolo/SplashActivity.java
@@ -3,19 +3,26 @@ package com.example.hyeminj.syolo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(),3000);
+    }
 
-        try{
-            Thread.sleep(4000);
-        }catch(InterruptedException e){
-            e.printStackTrace();
+    private class splashhandler implements Runnable {
+        public void run(){
+            startActivity(new Intent(getApplication(),MainActivity.class));
+            SplashActivity.this.finish();
         }
+    }
+    @Override
+    public void onBackPressed(){
 
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
     }
 }
